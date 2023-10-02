d4a8ad0f98feb336ceae411c6dcfb6ecba05b6c2
Joshua Jacobs <joshua.jacobs126@gmail.com>
2023-10-02 16:57:20 -0500

    Added Result pattern into project

---
+++ LibraryIdentityProvider/Patterns/ResultAndError/DomainValidationResult.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace LibraryIdentityProvider.Patterns.ResultAndError
+{
+    public class DomainValidationResult : Result, IValidationResult
+    {
+        private ErrorCode? _highLevelErrorCode;
+
+        private DomainValidationResult(ValidationError[] errors)
+            : base(false, IValidationResult.ValidationError(errors[0].Code.ErrorDomain, errors[0].Code.ErrorSpecification))
+        {
+            ValidationErrors = errors;
+        }
+
+        /// <summary>
+        /// Successful Validation Para
+        /// </summary>
+        /// <param name="success"></param>
+        /// <param name="errorType"></param>
+        private DomainValidationResult()
+            : base(true, Error.None)
+        {
+            ValidationErrors = Array.Empty<ValidationError>();
+        }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public ErrorCode? HighLevelErrorCode
+        {
+            get
+            {
+                if (_highLevelErrorCode != null && _highLevelErrorCode != ErrorCode.None)
+                {
+                    return _highLevelErrorCode;
+                }
+
+                if (ValidationErrors.Length > 0)
+                {
+                    return ValidationErrors[0].Code;
+                }
+
+                return null;
+            }
+
+            set
+            {
+                if (value is null)
+                {
+                    throw new InvalidOperationException("Cannot set HighLevelErrorCode to null");
+                }
+
+                _highLevelErrorCode = value;
+            }
+        }
+
+        public ValidationError[] ValidationErrors { get; }
+
+        public static new DomainValidationResult Success() { return SuccessfulValidation(); }
+
+        public static DomainValidationResult SuccessfulValidation() { return new DomainValidationResult(); }
+
+        public static DomainValidationResult WithErrors(ValidationError[] errors) { return new DomainValidationResult(errors); }
+    }
+}
---
+++ LibraryIdentityProvider/Patterns/ResultAndError/ValidationError.cs
@@ -0,0 +1,36 @@
+﻿namespace LibraryIdentityProvider.Patterns.ResultAndError
+{
+    /// <summary>
+    /// Class for Validation Errors.
+    /// </summary>
+    public class ValidationError : Error
+    {
+
+        /// <summary>
+        /// Validation Error for constructor.
+        /// </summary>
+        /// <param name="messageHeader">String to display in message before error. Note that a new line is inserted at the end.</param>
+        /// <param name="errorMessage">Error message for validation.</param>
+        public ValidationError(ErrorCode errorCode, string errorMessage)
+            : base(errorCode, errorMessage)
+        {
+            DetermineIfValidationLevelIsInternal(errorCode);
+        }
+
+        public ValidationError(ErrorCode errorCode, string errorMessage, bool isInternal)
+            : base(errorCode, errorMessage, isInternal)
+        {
+        }
+
+        /// <summary>
+        /// Determines if the error code type is an internal error. This can be used to mask errors when returned in the API.
+        /// </summary>
+        /// <param name="validationLevel">What level the error occurred at.</param>
+        /// <exception cref="InvalidOperationException">Throws if a <see cref="LibraryValidatorType.Mixed"/> is passed in. Errors
+        /// must </exception>
+        private void DetermineIfValidationLevelIsInternal(ErrorCode errorCode)
+        {
+            IsInternalError = false;
+        }
+    }
+}
