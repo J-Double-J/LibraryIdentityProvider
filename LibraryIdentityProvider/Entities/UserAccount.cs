c509c3c4b2f44f7471a82c9e19d6a449b156493a
Joshua Jacobs <joshua.jacobs126@gmail.com>
2023-10-03 07:36:25 -0500

    User Account added to Postgres

---
+++ LibraryIdentityProvider/Entities/UserAccount.cs
@@ -0,0 +1,31 @@
+﻿
+namespace LibraryIdentityProvider.Entities
+{
+    public class UserAccount : Entity
+    {
+        public string Username { get; private set; }
+        public string[] Claims { get; private set; }
+        public string Email { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        /// <summary>
+        /// Constructor for EFCore
+        /// </summary>
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        private UserAccount() : base(Guid.Empty)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+        }
+
+        public UserAccount(Guid guid, string username, string[] claims, string email, string firstName, string lastName)
+            : base(guid)
+        {
+            Username = username;
+            Claims = claims;
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
---
+++ LibraryIdentityProvider/Migrations/20231003123033_UserAccountAdded.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryIdentityProvider.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserAccountAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserAccount",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Claims = table.Column<string[]>(type: "text[]", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccount", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAccount");
+        }
+    }
+}
